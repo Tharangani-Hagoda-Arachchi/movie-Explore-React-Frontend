f3511a3849f779f71ead58f4fdf3c0d08ce42591
Tharangani Hagoda Arachchi <tharangani@Tharanganis-MacBook-Air.local>
2025-05-10 15:33:19 +0530

    Protected the route using token

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { CssBaseline } from "@mui/material";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginPage from "./pages/LoginPage";
 import HomePage from "./pages/HomePage";
+import PrivateRoute from "./privateRoutes/PrivateRoute";
 
 function App() {
   return (
@@ -11,7 +12,9 @@ function App() {
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/register" element={<RegistrationPage />} />
-        <Route path="/home" element={<HomePage />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/home" element={<HomePage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ src/privateRoutes/PrivateRoute.jsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
+import axios from 'axios';
+import {Box, CircularProgress} from "@mui/material"
+
+const PrivateRoute = () => {
+  const [auth, setAuth] = useState(null); // null = loading, false = not auth, true = auth
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      setAuth(false);
+      return;
+    }
+
+    axios.get('http://localhost:4000/api/auths/verify-token', {
+      headers: { Authorization: `Bearer ${token}` },
+    })
+    .then(() => setAuth(true))
+    .catch(() => setAuth(false));
+  }, []);
+
+  if (auth === null) 
+    return (
+       <Box
+         sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '100vh',
+        }}
+      >
+        <CircularProgress />
+      </Box>
+    )
+
+  return auth ? <Outlet /> : <Navigate to="/login" replace />;
+};
+
+export default PrivateRoute;
